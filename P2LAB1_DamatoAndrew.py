b40bda741f5436c8600929a3be035a7b23224d59
Andrew D'Amato <146122404+AndrewDamato@users.noreply.github.com>
2023-10-03 14:35:53 -0400

    Add files via upload
    First upload

---
+++ P2LAB1_DamatoAndrew.py
@@ -0,0 +1,17 @@
+#Andrew D'Amato
+#10/3/23
+#Use float and expressions to calculate gas cost
+
+#Create input variables
+mpg = float(input("Enter the car's mpg: "))
+cost_per_gallon = float(input("How much does a gallon of gas cost: "))
+
+#Calculate gas cost based off gallons needed & price per gallon
+#Calculate for 20miles, 75miles, 500 miles
+
+cost_20 = (20/mpg) * cost_per_gallon
+cost_75 = (75/mpg) * cost_per_gallon
+cost_500 = (500/mpg) * cost_per_gallon
+
+#Output values using f-string & format floats
+print (f"{cost_20:.2f} {cost_75:.2f} {cost_500:.2f}")
