7d39af63e258c1e55a765296d0afd70ef869f171
Andrew D'Amato <146122404+AndrewDamato@users.noreply.github.com>
2023-10-26 14:37:31 -0400

    it aint done

---
+++ P3HW2_DamatoAndrew.py
@@ -0,0 +1,21 @@
+#Andrew D'Amato
+#10/26/23
+#Uses if/else statements
+
+#Get employee name from user
+
+#Get number of hours from user
+
+#Get payrate per hour from user
+
+#Determine if employee worked more than 40 hours
+
+#Calculate reg hours worked
+
+#Calculate pay for reg hours
+
+#Calculate OT hours
+
+#Calculate OT pay
+
+#Display name, payrate, reg hours, OT hours, OT pay, gross pay
