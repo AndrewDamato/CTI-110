1ef958cd94388ae85a9625195b54beaffb268749
Andrew D'Amato <146122404+AndrewDamato@users.noreply.github.com>
2023-11-02 13:28:48 -0400

    finished

--- P3HW2_DamatoAndrew.py
+++ P3HW2_DamatoAndrew.py
@@ -1,21 +1,28 @@
-#Andrew D'Amato
-#10/26/23
-#Uses if/else statements
+# Andrew D'Amato
+# 10/26/23
+# Uses if/else statements
 
-#Get employee name from user
-
-#Get number of hours from user
-
-#Get payrate per hour from user
-
-#Determine if employee worked more than 40 hours
-
-#Calculate reg hours worked
-
-#Calculate pay for reg hours
-
-#Calculate OT hours
-
-#Calculate OT pay
-
-#Display name, payrate, reg hours, OT hours, OT pay, gross pay
+# Get employee name from user
+Name = input("Enter employee's name: \n  ")
+# Get number of hours from user
+Hours = int(input("Enter number of hours worked: \n  "))
+# Get pay rate per hour from user
+PayRate = float(input("Enter employee's pay rate: \n  "))
+# Determine if employee worked more than 40 hours
+if Hours > 40:
+    # Calculate OT hours
+    OTHours = Hours - 40
+    # Calculate reg hours worked
+    RegHours = Hours - OTHours
+    # Calculate pay for reg hours
+    RegPay = RegHours * PayRate
+    # Calculate OT pay
+    OTPay = OTHours * (PayRate * 1.5)
+    ## Calculate gross pay
+    GrossPay = RegPay + OTPay
+    # Display name, payrate, reg hours. OT hours, OT pay, gross pay
+    print("------------------------------------")
+    print(f"Employee Name: {Name}\n")
+    print("Hours Worked  |  Pay Rate  |  OverTime  |  OverTime Pay  |  RegHour Pay  |  Gross Pay")
+    print("-------------------------------------------------------------------------------------------")
+    print(f"     {Hours}       |     {PayRate}    |     {OTHours}     |    ${OTPay}     |    ${RegPay}     |    ${GrossPay}")
